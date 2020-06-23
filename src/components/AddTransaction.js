5917d19915404a300f38a20eff92dea86ccd8b5b
Arsi-Air <32060641+Arsi-Air@users.noreply.github.com>
2020-06-23 21:34:28 +0400

    up

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,28 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
 const [text, setText] = useState('');
 const [amount, setAmount] = useState(0);
 
+const { addTransaction } = useContext(GlobalContext);
+
+const onSubmit = e => {
+  e.preventDefault();
+
+  const newTransaction = {
+    id: Math.floor(Math.random() * 100000000),
+    text,
+    amount: +amount
+  }
+
+  addTransaction(newTransaction);
+}
+
     return (
         <>
             <h3>Add new transaction</h3>
-      <htmlForm>
+      <form onSubmit = {onSubmit}>
         <div className="htmlForm-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
@@ -20,7 +35,7 @@ const [amount, setAmount] = useState(0);
           <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
-      </htmlForm>
+      </form>
         </>
     )
 }
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,11 +1,17 @@
 export default (state, action) => {
-    switch(action.type) {
-        case 'DELETE_TRANSACTION':
-            return{
-                ...state,
-                transactions :state.transactions.filter(transaction => transaction.id !== action.payload)
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload)
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions]
+      }
+    default:
+      return state;
+  }
+};
