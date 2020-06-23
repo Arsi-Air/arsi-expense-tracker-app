b8658bf5c3ecd5cfce306122e4e5145bffe8a7a4
Arsi-Air <32060641+Arsi-Air@users.noreply.github.com>
2020-06-24 00:13:49 +0400

    Update Quotes.js

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 export const Quotes = () => {
-    const quoteStore = ["saas", "wqdqwd", "ssadas", "sadasd"]
+    const quoteStore = ["saassaassaassaassaassaassaassaassaassaassaassaassaas", "wqdqwd", "ssadas", "sadasd"]
     const num = Math.floor(Math.random()*4)
 
 
     return (
         <div>
-            {quoteStore[num]};
+            Financial tip of the day: {quoteStore[num]}
         </div>
     )
 }
